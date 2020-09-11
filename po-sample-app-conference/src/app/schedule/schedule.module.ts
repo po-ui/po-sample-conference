cf0a70143ca0f134ea4af4b6aa195df940a5340b
jhony.senem <jhonyeduardo96@gmail.com>
2020-09-11 00:13:10 -0300

    feat(schedule): implementa  a funcionalidade de favoritos

--- po-sample-app-conference/src/app/schedule/schedule.module.ts
+++ po-sample-app-conference/src/app/schedule/schedule.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
+import { PoTabsModule } from '@po-ui/ng-components';
+
 import { ScheduleComponent } from './schedule.component';
 import { ScheduleRoutingModule } from './schedule.routing';
 import { ScheduleFilterComponent } from './schedule-filter/schedule-filter.component';
+import { ScheduleListComponent } from './schedule-list/schedule-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    PoTabsModule,
     ScheduleRoutingModule
   ],
-  declarations: [ScheduleComponent, ScheduleFilterComponent]
+  declarations: [ScheduleListComponent, ScheduleComponent, ScheduleFilterComponent]
 })
 export class ScheduleModule { }
