1c82d85ca5c47acdd773b71d0d86cae8c9d9189e
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-10-19 22:47:50 -0300

    fix: correção de porta para produção

--- po-sample-api-conference/src/main.ts
+++ po-sample-api-conference/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
