99cd1eec4d8952c0e73fbf28af02fd64db181c17
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-11-26 10:00:50 -0300

    Correção

--- po-sample-app-conference/src/app/app-routing.module.ts
+++ po-sample-app-conference/src/app/app-routing.module.ts
@@ -56,7 +56,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule],
   providers: [SyncResolver]
